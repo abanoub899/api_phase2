e70b29cab7900f2c6360125087ed458781b680ad
abanoub899 <abanobgayed@outlook.com>
2021-07-11 14:47:03 +0200

    abanob

---
+++ database/migrations/2021_07_11_112802_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',120);
+            $table->string('email',50)->unique();
+            $table->string('password',70);
+            $table->string('phone',25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
